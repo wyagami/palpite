b5cfe0c35d99080f34e938147a73185504f1c876
wyagami <43221038+wyagami@users.noreply.github.com>
2025-02-12 11:39:58 -0300

    Add files via upload

--- palpite.py
+++ palpite.py
@@ -1,6 +1,19 @@
 import streamlit as st
 import random
 
+# Configurar o layout e título da página
+st.set_page_config(page_title="Advinhe o Numero", layout="centered")
+st.title("Advinhe o Numero")
+st.sidebar.header("Instruções")
+st.sidebar.write("""
+O jogo é chamado Adivinhe o Número. O computador escolhe um número aleatório entre 1 e 100, e o jogador tenta adivinhar
+o número. O jogo informa se o palpite está muito alto ou muito baixo até que o jogador acerte.
+- Boa sorte!
+- Caso tenha alguma idéia para publicarmos, envie uma mensagem para: 11-990000425 (Willian)
+- Contribua com qualquer valor para mantermos a pagina no ar. PIX (wpyagami@gmail.com)
+""")
+
+
 # Função principal do jogo
 def adivinhe_o_numero():
     st.title("🎮 Adivinhe o Número 🎮")
